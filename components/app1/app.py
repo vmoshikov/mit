c98236429007cf4f12197f4a0faf9c4109c73936
Vladislav Moshikov <vmoshikov@gmail.com>
2022-11-13 23:10:58 +0300

    Add core app in docker

--- components/app1/app.py
+++ components/app1/app.py
@@ -1,12 +1,21 @@
 from flask import Flask
+from core.routes import core
+from adapters.keycloak import auth
+from os import environ
+from keycloak.extensions.flask import AuthenticationMiddleware
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = environ.get('SECRET_KEY')
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+app.wsgi_app = AuthenticationMiddleware(
+        app.wsgi_app,
+        app.config,
+        app.session_interface,
+        callback_url=f"http://localhost/oidc_callback"
+)
 
+app.register_blueprint(core)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
