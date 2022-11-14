5e88c1063989345f92ea9203dddf338475642eff
Vladislav Moshikov <vmoshikov@gmail.com>
2022-11-14 14:07:48 +0300

    Fix keycloak logout, remove core directory, remove adapters directory

--- components/app1/app.py
+++ components/app1/app.py
@@ -1,7 +1,12 @@
-from flask import Flask
-from core.routes import core
-from adapters.keycloak import auth
+import json
+import httpx
 from os import environ
+
+from flask import Flask
+from flask import session, render_template, current_app, redirect
+from keycloak import Client
+from keycloak.config import config
+from keycloak.utils import auth_header
 from keycloak.extensions.flask import AuthenticationMiddleware
 
 app = Flask(__name__)
@@ -12,10 +17,57 @@ app.wsgi_app = AuthenticationMiddleware(
         app.wsgi_app,
         app.config,
         app.session_interface,
-        callback_url=f"http://localhost/oidc_callback"
+        callback_url=f"http://localhost/oidc_callback",
 )
 
-app.register_blueprint(core)
+kc = Client()
+
+@app.route('/')
+def index():
+    current_app.logger.debug(f"Keycloak user: {kc.username}")
+    result = dict()
+    current_app.logger.debug('We in core page in first app')
+    if session.get('user'):
+        current_app.logger.debug('We has user info')
+        result['user'] = session
+    return render_template('home.html', result=result)
+
+
+@app.route("/oidc_callback")
+def oidc_callback():
+    user = session["user"]
+    return f"Howdy {user}"
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    """ Initiate authentication """
+    tokens = json.loads(session.get('tokens'))
+    config.openid.end_session_endpoint = "http://keycloak:8080/auth/realms/CoreRealm/protocol/openid-connect/logout"
+    if tokens:
+        kc.logout(tokens.get('access_token'), tokens.get('refresh_token'))
+        payload = {
+            "client_id": config.client.client_id,
+            "client_secret": config.client.client_secret,
+            "refresh_token": tokens.get('refresh_token'),
+        }
+        headers = auth_header(tokens.get('access_token'))
+        # log.debug("Logging out user from server")
+        httpx.post(
+                config.openid.end_session_endpoint, data=payload, headers=headers
+        )
+        session['state'] = None
+        session['user'] = None
+    return redirect('/')
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    """ Initiate authentication """
+    url, state = kc.login()
+    session['state'] = state
+    return redirect(url)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
